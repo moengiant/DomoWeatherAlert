7454d54d93fd17ad1f5c32c785e529cad6dbfe19
moengiant <davidstephenson@wowway.com>
2018-01-15 20:09:16 -0600

    Add files via upload

---
+++ script_time_weather.lua
@@ -0,0 +1,7 @@
+commandArray = {}
+
+cmd ='python "C:\\Program files (x86)\\Domoticz\\scripts\\python\\script_time_weatheralert.py"'
+
+os.execute(cmd)
+print ('Running Weather Alert Check')
+return commandArray
